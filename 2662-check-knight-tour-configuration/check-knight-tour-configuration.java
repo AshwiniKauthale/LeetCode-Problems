fe69e6ae84ca4ace196c985514d8b847fbbf73e8
AshwiniKauthale <kauthaleashwini40@gmail.com>
2026-05-22 16:50:08 +0530

    Time: 1 ms (96.03%) | Memory: 46.5 MB (27.44%) - LeetSync

---
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
@@ -0,0 +1,91 @@
+class Solution {
+
+    public boolean helper(int grid[][],int row, int col,int num)
+    {
+        int n = grid.length;
+        int i = 0;
+        int j = 0;
+
+        if(grid[row][col] == n*n -1)
+        {
+            return true;
+        }
+
+        i = row - 2;
+        j = col + 1;
+
+        if(i >= 0 && j < n && grid[i][j] == num + 1)
+        {
+            return helper(grid,i,j,num+1);
+        }
+
+        i = row - 2;
+        j = col - 1;
+
+        if(i >= 0 && j >= 0 && grid[i][j] == num + 1)
+        {
+            return helper(grid,i,j,num+1);
+        }
+
+        i = row + 2;
+        j = col - 1;
+
+        if(i < n  && j >= 0 && grid[i][j] == num + 1)
+        {
+            return helper(grid,i,j,num+1);
+        }
+
+        i = row + 2;
+        j = col + 1;
+
+        if(i < n && j < n && grid[i][j] == num + 1)
+        {
+            return helper(grid,i,j,num+1);
+        }
+
+        i = row + 1;
+        j = col + 2;
+
+        if(i < n && j < n && grid[i][j] == num + 1)
+        {
+            return helper(grid,i,j,num+1);
+        }
+        
+
+        i = row - 1;
+        j = col + 2;
+
+        if(i >= 0 && j < n && grid[i][j] == num + 1)
+        {
+            return helper(grid,i,j,num+1);
+        }
+
+        i = row + 1;
+        j = col - 2;
+
+        if(i < n && j >= 0 && grid[i][j] == num + 1)
+        {
+            return helper(grid,i,j,num+1);
+        }
+
+        i = row - 1;
+        j = col - 2;
+
+        if(i >= 0 && j >= 0 && grid[i][j] == num + 1)
+        {
+            return helper(grid,i,j,num+1);
+        }
+
+        return false;
+    }
+
+    public boolean checkValidGrid(int[][] grid)
+    {
+        if(grid[0][0] != 0)
+        {
+            return false;
+        }    
+
+        return helper(grid,0,0,0);
+    }
+}
