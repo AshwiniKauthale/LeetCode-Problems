592e1111b6909981878d1ba60d5f0a4c6e949151
AshwiniKauthale <kauthaleashwini40@gmail.com>
2026-07-28 20:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (8.76%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target)
+    {
+        boolean bFlag = false;
+        int i = 0;
+        int j = 0;
+
+        for(i = 0; i < matrix.length; i++)
+        {
+            for(j = 0; j < matrix[i].length; j++)
+            {
+                if(matrix[i][j] == target)
+                {
+                    bFlag = true;
+                    break;
+                }
+            }
+        }   
+
+        return bFlag; 
+    }
+}
